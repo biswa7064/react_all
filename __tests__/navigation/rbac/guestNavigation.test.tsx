572d03a737f1c66729962f3c8b349d99aa5d7739
biswa7064 <biswaranjan16856@gmail.com>
2024-10-10 09:43:01 +0530

    test: start navigation testing

---
+++ __tests__/navigation/rbac/guestNavigation.test.tsx
@@ -0,0 +1,10 @@
+// import HomePage from '@/app/page'
+describe("Guest Navigation", () => {
+	// before/after test scenarios
+	// test which pages role:guest could visit
+	it("should test successful access of pages for role:guest", () => {
+		// render(<HomePage/>)
+	})
+	// test restricted pages for role:guest
+	// test redirect to login page if role:guest try to access the restricted pages
+})
