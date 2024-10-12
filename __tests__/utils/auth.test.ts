b9a93cdf2f15062e5c6e3d49c3db5d6508f869e5
biswa7064 <biswaranjan16856@gmail.com>
2024-10-12 20:36:38 +0530

    test: finish test for utils-auth-redirectToLogin

--- __tests__/utils/auth.test.ts
+++ __tests__/utils/auth.test.ts
@@ -1,26 +1,27 @@
 import { redirectToLogin } from "@/utils/auth"
-import { act, waitFor } from "@testing-library/react"
 import { redirect, RedirectType } from "next/navigation"
 jest.mock("next/navigation")
 const mockRedirect = jest.mocked(redirect)
 describe("util:redirectToLogin", () => {
-	beforeEach(() => {
-		jest.resetAllMocks()
-	})
 	afterEach(() => {
 		jest.clearAllMocks()
 	})
+	it("should redirect to login page without returnUrl", async () => {
+		await redirectToLogin()
+		expect(redirect).toHaveBeenCalledWith("/login?returnUrl=", "replace")
+	})
 	it("should call redirect method with returnUrl", async () => {
 		const returnUrl = "/return-url"
-		await act(async () => {
-			redirectToLogin(returnUrl)
-		})
-		await waitFor(() => {
-			expect(mockRedirect).toHaveBeenCalledTimes(1)
-			expect(mockRedirect).toHaveBeenCalledWith(
-				`/login?returnUrl=${encodeURIComponent(returnUrl)}`,
-				RedirectType.replace
-			)
-		})
+		await redirectToLogin(returnUrl)
+		expect(mockRedirect).toHaveBeenCalledTimes(1)
+		expect(mockRedirect).toHaveBeenCalledWith(
+			`/login?returnUrl=${encodeURIComponent(returnUrl)}`,
+			RedirectType.replace
+		)
+	})
+
+	it("should handle empty string returnUrl", async () => {
+		await redirectToLogin("")
+		expect(redirect).toHaveBeenCalledWith("/login?returnUrl=", "replace")
 	})
 })
