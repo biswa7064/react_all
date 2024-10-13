a8ceb9836d588e47072c9c7b888fdc59b1517c53
biswa7064 <biswaranjan16856@gmail.com>
2024-10-13 12:06:07 +0530

    test: update jest.setup for conole.log

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,8 @@
 require("@testing-library/jest-dom")
-// const consoleLogSpy = jest.spyOn(console, "log")
-// beforeAll(() => {
-// 	consoleLogSpy.mockImplementation(() => jest.fn())
-// })
-// afterAll(() => {
-// 	consoleLogSpy.mockRestore()
-// })
+const consoleLogSpy = jest.spyOn(console, "log")
+beforeAll(() => {
+	consoleLogSpy.mockImplementation(() => jest.fn())
+})
+afterAll(() => {
+	consoleLogSpy.mockRestore()
+})
