7b2d10f6cf874174cac3722f258f2703ae779795
biswa7064 <biswaranjan16856@gmail.com>
2024-12-01 17:39:21 +0530

    feat: implement source mapping used for debug effectively

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require("webpack")
 module.exports = {
 	entry: "./src/app/page.tsx",
 	mode: "development",
+	// devtool: "eval-source-map", // we can create source-map using devtool option
 	devServer: {
 		static: { directory: path.join(__dirname, "dist") },
 		port: 4444,
@@ -28,6 +29,11 @@ module.exports = {
 		new webpack.DllReferencePlugin({
 			context: process.cwd(),
 			manifest: require("./dist/reactpath.json")
+		}),
+		// also by using plugin we can implement source-map
+		// we can have more control on source map if created using plugin
+		new webpack.SourceMapDevToolPlugin({
+			filename: "appcode.map"
 		})
 	]
 }
