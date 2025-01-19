ab341030ba3ba354a498c53ccd8d943ad9a59ac0
biswa7064 <biswaranjan16856@gmail.com>
2025-01-19 19:14:22 +0530

    feat: create mocks for axios and next.config

---
+++ __mocks__/axios.ts
@@ -0,0 +1,25 @@
+const mockAxios = {
+	get: jest.fn(() => Promise.resolve({ data: {} })),
+	post: jest.fn(() => Promise.resolve({ data: {} })),
+	put: jest.fn(() => Promise.resolve({ data: {} })),
+	delete: jest.fn(() => Promise.resolve({ data: {} })),
+	patch: jest.fn(() => Promise.resolve({ data: {} })),
+	create: jest.fn(function () {
+		return this
+	}),
+	defaults: {
+		adapter: {}
+	},
+	interceptors: {
+		request: {
+			use: jest.fn(),
+			eject: jest.fn()
+		},
+		response: {
+			use: jest.fn(),
+			eject: jest.fn()
+		}
+	}
+}
+
+export default mockAxios
--- __mocks__/next/config.ts
+++ __mocks__/next/config.ts
@@ -1,11 +1,9 @@
 import getConfig from "next/config"
-export const mockGetConfig = jest.fn()
-jest.mock("next/config", () => () => ({
-	getConfig: mockGetConfig.mockReturnValue({
-		publicRuntimeConfig: {
-			apiBaseUrl: "/"
-		}
-	})
-}))
+export const mockGetConfig = jest.fn().mockReturnValue({
+	publicRuntimeConfig: {
+		apiBaseUrl: "/"
+	}
+})
+jest.mock("next/config", () => () => mockGetConfig())
 
 export default getConfig
